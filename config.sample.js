01ddb9d85fbf96f31fcb959b0b3ca9a6171ed8de
Victor Wang <icesoar@hotmail.com>
2014-08-04 00:24:09 +0800

    update config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -4,29 +4,78 @@
 "use strict";
 
 module.exports = {
-  tempDir : '',
-  host    : 'yantoo.net',
-  session : {
-    type : 'redis',
-    redis: {
-      host: 'localhost',
-      port: 6379
+  development: {
+    tempDir : '',
+    host    : 'yantoo.net',
+    session : {
+      type : 'redis',
+      redis: {
+        host: 'localhost',
+        port: 6379
+      }
+    },
+    database: {
+      type : 'mysql',
+      mysql: {
+        host    : 'localhost',
+        port    : 3306,
+        schema  : 'much',
+        user    : 'much_admin',
+        password: ''
+      }
+    },
+    storage : {
+      type: 'oss',
+      oss : {
+        accessKeyId    : 'ACC_KEY_ID',
+        accessKeySecret: 'ACC_KEY_SC',
+        bucket         : 'BUCKET.NAME',
+        region         : 'OSS.LOCATION.CONSTRAINT',
+        boundHost      : 'CUSTOM.BOUND.HOST.DOMAIN'
+      }
+    },
+    social  : {
+      weibo: {
+        appKey   : 'APP.KEY',
+        appSecret: 'APP.SECRET'
+      }
     }
   },
-  database: {
-    type : 'mysql',
-    mysql: {
-      host    : 'localhost',
-      port    : 3306,
-      schema  : 'much',
-      user    : 'much_admin',
-      password: ''
-    }
-  },
-  social  : {
-    weibo: {
-      appKey   : 'APP.KEY',
-      appSecret: 'APP.SECRET'
+  production : {
+    tempDir : '',
+    host    : 'yantoo.net',
+    session : {
+      type : 'redis',
+      redis: {
+        host: 'localhost',
+        port: 6379
+      }
+    },
+    database: {
+      type : 'mysql',
+      mysql: {
+        host    : 'localhost',
+        port    : 3306,
+        schema  : 'much',
+        user    : 'much_admin',
+        password: ''
+      }
+    },
+    storage : {
+      type: 'oss',
+      oss : {
+        accessKeyId    : 'ACC_KEY_ID',
+        accessKeySecret: 'ACC_KEY_SC',
+        bucket         : 'BUCKET.NAME',
+        region         : 'OSS.LOCATION.CONSTRAINT',
+        boundHost      : 'CUSTOM.BOUND.HOST.DOMAIN'
+      }
+    },
+    social  : {
+      weibo: {
+        appKey   : 'APP.KEY',
+        appSecret: 'APP.SECRET'
+      }
     }
   }
 };
